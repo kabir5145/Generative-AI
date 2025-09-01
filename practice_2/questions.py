4d9f07d99b672d91dc718c543dc0028870f3bb16
kabir5145 <kabir10112006@gmail.com>
2025-09-01 19:47:48 +0530

    qustion 6

--- practice_2/questions.py
+++ practice_2/questions.py
@@ -1,24 +1,27 @@
 # question 1
-n = 10
-m = n+1
-n = m+1
-print(m==n)
-print(n==10)
-print(m==n+1)
-print(n==n+1)
+# n = 10
+# m = n+1
+# n = m+1
+# print(m==n)
+# print(n==10)
+# print(m==n+1)
+# print(n==n+1)
 
-# question 2
-x = 2
-print( (x/2)==(x//2) )
+# # question 2
+# x = 2
+# print( (x/2)==(x//2) )
 
-# question 3
-y = 2
-print( y+y*y==(y+y)*y)
+# # question 3
+# y = 2
+# print( y+y*y==(y+y)*y)
 
-# question 4
-z = '1'
-print(z+z*2==(z+z)*2)
+# # question 4
+# z = '1'
+# print(z+z*2==(z+z)*2)
 
-# question 5
-g = '0'
-print(g+t==g+t)
+# # question 5
+# g = '0'
+# print(g+t==g+t)
+
+# question 6
+print(ord('A'))
