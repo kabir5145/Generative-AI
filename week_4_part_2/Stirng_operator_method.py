e1a0cb2336fb93bcd26fc2d2234be6fecb2b4c7c
kabir5145 <kabir10112006@gmail.com>
2025-09-29 10:53:17 +0530

    String slicing

--- week_4_part_2/Stirng_operator_method.py
+++ week_4_part_2/Stirng_operator_method.py
@@ -1,8 +1,15 @@
 # in operator
-name = (input('Your name: '))
+# name = (input('Your name: '))
 
-has_kab = 'kab' in name
-has_lower = 'ir'.lower() in name
+# has_kab = 'kab' in name
+# has_lower = 'ir'.lower() in name
 
-print(has_kab)
-print(has_lower)
+# print(has_kab)
+# print(has_lower)
+
+# String slicing
+s = 'cat'
+print(s[1:2])
+print(s[1:])
+print(s[:2])
+print(s[:-1])
