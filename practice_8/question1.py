49523ffb4ae3ca9ec9dbb29ba26e521292b0d16d
kabir5145 <kabir10112006@gmail.com>
2025-10-31 22:30:27 +0530

    solving questions

---
+++ practice_8/question1.py
@@ -0,0 +1,8 @@
+def factorial(n: int) -> int:
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+if __name__ == "__main__":
+    n = 4
+    print(factorial(n))
---
+++ practice_8/question_1.c
@@ -0,0 +1,5 @@
+int main() {
+    // Print a message on the screen
+    printf("Hello, World!\n");
+    return 0;
+}
